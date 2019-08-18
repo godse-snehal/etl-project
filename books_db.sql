b10f0ec61a318003b29705f6a4e44382d0472836
Nitha Balakrishnan <nitha.balakrishnan83@gmail.com>
2019-08-17 23:38:11 -0700

    updated the books_db.sql with amazon data queries

--- books_db.sql
+++ books_db.sql
@@ -18,3 +18,16 @@ DROP TABLE goodreads;
 
 
 
+CREATE TABLE all_books (
+	book_id INT Primary Key,
+	author TEXT,
+	rating varchar (40),
+	no_of_reviews INT,
+	title TEXT );
+	
+drop table all_books;
+
+select * from all_books
+
+
+
